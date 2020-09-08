39864ef45141516a88696a8bfb93fc6ebd48fa7e
kounaien <1127fujihiroto@gmail.com>
2020-09-08 21:47:45 +0900

    fix

--- node-sample/app.js
+++ node-sample/app.js
@@ -1,4 +1,4 @@
 
 const tutorial = require('./tutorial'); //slash means that it's within the same exact path that this file is currently in
 console.log(tutorial);
-console.log(tutorial(1,1));
+// console.log(tutorial(1,1));
--- node-sample/tutorial.js
+++ node-sample/tutorial.js
@@ -1,2 +1,10 @@
 const sum = (num1, num2) => num1 + num2;
-module.exports = sum;
+const PI = 3.14;
+class SomeMathObject{
+    constructor(){
+    console.log('object created');
+    }
+};
+module.exports.sum = sum;
+module.exports.PI = PI;
+module.exports.SomeMathObject = SomeMathObject;
