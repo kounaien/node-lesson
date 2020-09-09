f061e2be38e5421e0e6191f907da1625532e1936
kounaien <1127fujihiroto@gmail.com>
2020-09-09 22:17:52 +0900

    npm init

--- node-sample/app.js
+++ node-sample/app.js
@@ -202,9 +202,11 @@
 // server.listen('3000');
 
 
-const http = require('http');
-const fs = require('fs');
-http.createServer((req, res)=>{
-    const readStream = fs.createReadStream('./static/index.html');
-    res.writeHead(200, {'Content-type': 'text/html'});
-}).listen(3000);
+// const http = require('http');
+// const fs = require('fs');
+// http.createServer((req, res)=>{
+//     const readStream = fs.createReadStream('./static/index.html');
+//     res.writeHead(200, {'Content-type': 'text/html'});
+//     readStream.pipe(res);
+// }).listen(3000);
+
