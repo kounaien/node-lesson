d9f734cf67cf6a96d254b3049ba2e29317f49b38
kounaien <1127fujihiroto@gmail.com>
2020-09-16 01:36:06 +0900

    express install on vagrant solution is express install globally and configure PATH

--- node-sample/app.js
+++ node-sample/app.js
@@ -213,3 +213,14 @@
 // const _ = require('lodash');
 // let example = _.fill([1,2,3,4,5], "banana", 1, 4);
 // console.log(example);
+
+const express = require('express');
+const  app = express();
+
+app.get('/', (req, res)=> {
+    res.send('hello world');
+});
+
+app.listen(3000);
+
+
