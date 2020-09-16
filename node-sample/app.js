fdf0795e0aef71e5acec17aeb9ac92aacd9022d6
kounaien <1127fujihiroto@gmail.com>
2020-09-16 23:04:47 +0900

    joi

--- node-sample/app.js
+++ node-sample/app.js
@@ -215,10 +215,44 @@
 // console.log(example);
 
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const Joi = require('joi');
 const  app = express();
 
+
+app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res)=> {
-    res.send('hello world');
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.post('/',(req, res)=>{
+    console.log(req.body);
+    const schema = Joi.object().keys({
+        email : Joi.string().trim().email().required(),
+        password : Joi.string().min(5).max(10).required()
+    });
+    Joi.validate(req.body,schema, (err,result)=>{
+        if(err){
+            console.log(err);
+            res.send('an error has occurred');
+        }
+        console.log(result);
+        res.send('successfully send data')
+    })
+});
+
+app.get('/example', (req, res)=>{
+    res.send('hitting example route');
+});
+
+app.get('/example/:name/:age', (req, res)=> {
+    console.log(req.params);
+    console.log(req.query);
+    res.send(req.params.name + " : " + req.params.age);
 });
 
 app.listen(3000);
