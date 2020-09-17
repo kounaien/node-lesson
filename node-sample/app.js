18a7bbd2f62df777497da14f31889d3822946ea1
kounaien <1127fujihiroto@gmail.com>
2020-09-17 18:07:59 +0900

    finish youtube lesson

--- node-sample/app.js
+++ node-sample/app.js
@@ -214,53 +214,135 @@
 // let example = _.fill([1,2,3,4,5], "banana", 1, 4);
 // console.log(example);
 
+// const express = require('express');
+// const path = require('path');
+// const bodyParser = require('body-parser');
+// const Joi = require('joi');
+// const  app = express();
+
+
+// app.use('/public', express.static(path.join(__dirname, 'static')));
+// app.use(bodyParser.urlencoded({extended: false}));
+// // app.use(bodyParser.json());
+
+// app.get('/', (req, res)=> {
+//     res.sendFile(path.join(__dirname, 'static', 'index.html'));
+// });
+
+// app.post('/',(req, res)=>{
+//     console.log(req.body);
+//     // const schema = Joi.object().keys({
+//     //     email : Joi.string().trim().email().required(),
+//     //     password : Joi.string().min(5).max(10).required()
+//         const schema = Joi.object({
+//             email : Joi.string().trim().email().required(),
+//             password : Joi.string().min(5).max(10).required()
+//         });
+    
+//         const result = schema.validate(req.body)
+
+//         if(result.error){
+//             console.log(result.error);
+//             res.send('an error has occured');
+//         }
+//         else{
+//             console.log(result);
+//             res.send('successfully send data')
+//         };
+// });
+
+// app.get('/example', (req, res)=>{
+//     res.send('hitting example route');
+// });
+
+// app.get('/example/:name/:age', (req, res)=> {
+//     console.log(req.params);
+//     console.log(req.query);
+//     res.send(req.params.name + " : " + req.params.age);
+// });
+
+// app.listen(3000);
+
+
+// const Joi = require('joi')
+
+// const arrayString = ['banana', 'bacon', 'chesee'];
+// const arrayObjects = [{example: 'example1'}, {example: 'example2'}]
+
+// const userInput = { personalInfo: {
+//         streerAddress: '12345678',
+//         city: 'kfjoai',
+//         state: 'fj'
+//     },
+//     preferences: arrayString};
+
+
+// const personalInfoSchema = Joi.object().keys({
+//     streetAddress: Joi.string().trim().required(),
+//     city: Joi.string().trim().required(),
+//     state: Joi.string().trim().length(2).required()
+// })
+
+// const preferencesSchema = Joi.array().items(Joi.object().keys({
+//     example: Joi.string().trim().required()
+// }));
+
+// const schema = Joi.object().keys({
+//     personalInfo: personalInfoSchema,
+//     preferences: preferencesSchema
+// });
+
+// const result = schema.validate(userInput)
+
+// if(result.err)
+//     console.log(result.err);
+// else(result)
+//     console.log(result);
+
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// app.use('/public', express.static(path.join(__dirname, 'static')));
+// app.set("view engine", "ejs");
+// app.get('/:userQuery', (req,res)=>{
+//     res.render('index', {data: {userQuery: req.params.userQuery,
+//         searchResults : ['book', 'book2', 'book3'],
+//         loggedIn: true,
+//         username: 'jfioew'
+//     }});
+// });
+
+// app.listen(3000);
+
+// const express = require('express');
+// const bodyparser = require('body-parser');
+// const app = express();
+
+// app.use(bodyparser.json());
+// app.use((req,res,next)=>{
+//     req.banana = 'banana'
+//     console.log(req.url,req.method);
+//     next();
+// });
+
+// app.get('/', (req,res)=>{
+//     console.log(req.banana);
+//     res.send('MiddleWare');
+// });
+
+// app.listen(3000);
+
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const Joi = require('joi');
-const  app = express();
+const app = express();
 
+app.use('/public', express.static(path.join(__dirname,'static')));
+app.set('view engine', 'ejs');
 
-app.use('/public', express.static(path.join(__dirname, 'static')));
-app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
+const people = require('./routes/people');
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'static', 'index.html'));
-});
-
-app.post('/',(req, res)=>{
-    console.log(req.body);
-    // const schema = Joi.object().keys({
-    //     email : Joi.string().trim().email().required(),
-    //     password : Joi.string().min(5).max(10).required()
-        const schema = Joi.object({
-            email : Joi.string().trim().email().required(),
-            password : Joi.string().min(5).max(10).required()
-        });
-    
-        const result = schema.validate(req.body)
-
-        if(result.err){
-            console.log(err);
-            res.send('an error has occured');
-        }
-        else{
-            console.log(result);
-            res.send('successfully send data')
-        };
-});
-
-app.get('/example', (req, res)=>{
-    res.send('hitting example route');
-});
-
-app.get('/example/:name/:age', (req, res)=> {
-    console.log(req.params);
-    console.log(req.query);
-    res.send(req.params.name + " : " + req.params.age);
-});
+app.use('/people', people);
 
 app.listen(3000);
-
-
