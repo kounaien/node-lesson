fad81c6ec79aad888e8f17ca8774914482abd209
kounaien <1127fujihiroto@gmail.com>
2020-09-17 14:10:02 +0900

    learn how to use joi

--- node-sample/app.js
+++ node-sample/app.js
@@ -223,7 +223,7 @@ const  app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, 'static', 'index.html'));
@@ -231,18 +231,24 @@ app.get('/', (req, res)=> {
 
 app.post('/',(req, res)=>{
     console.log(req.body);
-    const schema = Joi.object().keys({
-        email : Joi.string().trim().email().required(),
-        password : Joi.string().min(5).max(10).required()
-    });
-    Joi.validate(req.body,schema, (err,result)=>{
-        if(err){
+    // const schema = Joi.object().keys({
+    //     email : Joi.string().trim().email().required(),
+    //     password : Joi.string().min(5).max(10).required()
+        const schema = Joi.object({
+            email : Joi.string().trim().email().required(),
+            password : Joi.string().min(5).max(10).required()
+        });
+    
+        const result = schema.validate(req.body)
+
+        if(result.err){
             console.log(err);
-            res.send('an error has occurred');
+            res.send('an error has occured');
         }
-        console.log(result);
-        res.send('successfully send data')
-    })
+        else{
+            console.log(result);
+            res.send('successfully send data')
+        };
 });
 
 app.get('/example', (req, res)=>{
